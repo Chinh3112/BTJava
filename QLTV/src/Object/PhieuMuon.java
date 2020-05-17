30864a842118a776067763838395bdc5cf6c6fa3
chinh <chinh.healthchain@gmail.com>
2020-05-17 13:45:15 +0700

    update

---
+++ QLTV/src/Object/PhieuMuon.java
@@ -0,0 +1,83 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Object;
+
+import java.sql.Date;
+
+/**
+ *
+ * @author Gaara
+ */
+public class PhieuMuon {
+    private String id;
+    private String id_khachhang;
+    private String id_sach;
+    private Date ngayMuon;
+    private Date hanTra;
+    private int status;
+    
+    public PhieuMuon() {
+        
+    }
+    
+    public PhieuMuon(String id, String id_khachhang, String id_sach, Date ngayMuon, Date hanTra, int status) {
+        this.id = id;
+        this.id_khachhang = id_khachhang;
+        this.id_sach = id_sach;
+        this.ngayMuon = ngayMuon;
+        this.hanTra = hanTra;
+        this.status = status;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getId_khachhang() {
+        return id_khachhang;
+    }
+
+    public void setId_khachhang(String id_khachhang) {
+        this.id_khachhang = id_khachhang;
+    }
+
+    public String getId_sach() {
+        return id_sach;
+    }
+
+    public void setId_sach(String id_sach) {
+        this.id_sach = id_sach;
+    }
+
+    public Date getNgayMuon() {
+        return ngayMuon;
+    }
+
+    public void setNgayMuon(Date ngayMuon) {
+        this.ngayMuon = ngayMuon;
+    }
+
+    public Date getHanTra() {
+        return hanTra;
+    }
+
+    public void setHanTra(Date hanTra) {
+        this.hanTra = hanTra;
+    }
+
+    public int getStatus() {
+        return status;
+    }
+
+    public void setStatus(int status) {
+        this.status = status;
+    }
+    
+}
